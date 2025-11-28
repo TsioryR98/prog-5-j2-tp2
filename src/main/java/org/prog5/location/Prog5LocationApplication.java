35267b361a3049656c1f8c346dd624dbb517f47a
TsioryR98 <hei.tsiory.2@gmail.com>
2025-11-28 06:52:58 +0300

    feat : project structure

---
+++ src/main/java/org/prog5/location/Prog5LocationApplication.java
@@ -0,0 +1,13 @@
+package org.prog5.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog5LocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog5LocationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/prog5/location/Prog5LocationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prog5.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog5LocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
